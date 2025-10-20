ca87f8a48ecb3750adc99977b133bf4f6cc16a41
Hassan <ch31389@gmail.com>
2025-10-21 02:07:53 +0500

    FEAT: Add final health check endpoint for Render

--- backend_django/urls.py
+++ backend_django/urls.py
@@ -1,10 +1,11 @@
-# core/urls.py (or your_project_name/urls.py)
-# CORRECT
-from django.urls import path, include
 from django.contrib import admin
-from django.urls import path, include # Make sure to import 'include'
+from django.urls import path, include # Make sure 'include' is imported
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')), # Add this line
+    
+    # This line tells Django to send any request that starts with ''
+    # to be handled by the urls.py file inside your 'api' app.
+    # This is how your health check at the root path will be found.
+    path('', include('api.urls')),
 ]
